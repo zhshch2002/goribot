8b6ca4b5562a9cae7e90b630d064765a1f336dc9
zhshch <zhshch@athorx.com>
2019-09-13 12:44:48 +0800

    add:DisallowHostPipeline

--- goribotExts/AllowHostPipeline.go
+++ goribotExts/AllowHostPipeline.go
@@ -25,3 +25,24 @@ func (s *AllowHostPipeline) OnRequest(spider *goribot.Spider, request *goribot.R
 	}
 	return request
 }
+
+type DisallowHostPipeline struct {
+	goribot.Pipeline
+	Hosts map[string]struct{}
+}
+
+func NewDisallowHostPipeline(hosts ...string) *AllowHostPipeline {
+	p := &AllowHostPipeline{}
+	p.Hosts = make(map[string]struct{})
+	for _, h := range hosts {
+		p.Hosts[strings.ToLower(h)] = struct{}{}
+	}
+	return p
+}
+
+func (s *DisallowHostPipeline) OnRequest(spider *goribot.Spider, request *goribot.Request) *goribot.Request {
+	if _, ok := s.Hosts[strings.ToLower(request.Url.Host)]; ok {
+		return nil
+	}
+	return request
+}
